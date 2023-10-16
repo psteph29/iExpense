82e9c795e606f11c0a4ee84f734d47a0840b6672
psteph29 <paigestephenson29@gmail.com>
2023-10-16 11:40:56 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Paige Stephenson on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

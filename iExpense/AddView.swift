98e0c507778be64750b574b1063322fed8d67a7e
psteph29 <paigestephenson29@gmail.com>
2023-10-17 14:44:53 -0600

    first commit

---
+++ iExpense/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Paige Stephenson on 10/16/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Paige Stephenson on 10/16/23.
+//
+
+import Foundation
